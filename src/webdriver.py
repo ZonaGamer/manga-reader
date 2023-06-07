2810471cd15f3f83bab72f146efcb87799298716
ZonaGamer <65832547+ZonaGamer@users.noreply.github.com>
2023-06-06 22:57:35 -0600

    Add files via upload

--- src/webdriver.py
+++ src/webdriver.py
@@ -3,7 +3,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.service import Service
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from threading import Thread
 
